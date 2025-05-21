a81e53f24ebb329192806637ffe89ecf4d74ed97
josephsmendoza <josephsmendoza@gmail.com>
2025-05-21 08:55:45 -0700

    initial commit

---
+++ GameDir/mod/zeta/Offline Development.lua
@@ -0,0 +1,45 @@
+local this={
+	modName = "Offline Development",
+	modDesc = "This mod enables online-only items for offline development and changes development requirements to better match an offline mode playthrough",
+	modCategory = {"Gameplay"},
+	modAuthor = {"SugoiDogo"},
+	modDisabledByDefault = false
+}
+
+local onlineOnly='p72'
+local minutes='p71'
+local resource2Usage='p68'
+local resource1Usage='p66'
+local level2='p64'
+local resource2='p61'
+local resource1='p59'
+local level1='p57'
+local level='p55'
+local gmpUsage='p54'
+local gmp='p53'
+local grade='p52'
+
+function this.EquipDevelopFlowSettingEntry(entry)
+    -- enable all equipment offline
+    entry[onlineOnly]=0
+    -- new level requirements max out at level 61 (100 S rank soldiers per unit)
+    entry[level]=4*(entry[grade]-1)+1
+    entry[level1]=4*(entry[grade]-1)+1
+    entry[level2]=4*(entry[grade]-1)+1
+    -- these are grade 1 cosmetic handguns, copy burkov costs
+    if entry[gmp] == 0 then
+        entry[gmp]=2e4
+        entry[gmpUsage]=entry[gmp]/100
+    end
+    -- these are online items, so give them high but reachable costs
+    if entry[gmp] == 100 or entry[gmp] > 5e6 then
+        entry[gmp]=3e5*entry[grade]
+        entry[gmpUsage]=entry[gmp]/100
+        entry[resource1]=2e3*entry[grade]
+        entry[resource1Usage]=entry[resource1]/10
+        entry[resource2]=2e3*entry[grade]
+        entry[resource2Usage]=entry[resource2]/10
+    end
+end
+
+return this
---
+++ README.md
@@ -0,0 +1,9 @@
+# Offline Development
+This is a mod for [Metal Gear Solid V: The Phantom Pain](https://store.steampowered.com/app/287700/)
+
+This mod requires [Zeta](https://www.nexusmods.com/metalgearsolidvtpp/mods/1309)
+
+This mod enables online-only items for offline development and changes development requirements to better match an offline mode playthrough.
+
+- All items have their level requirements changed to max out at level 61 for grade 15 gear, which is the level reached with 100 S-rank crew in each department.
+- Online items which don't normally have proper development requirements set by the game for offline mode have been given costs calculated based on their equipment grade.
